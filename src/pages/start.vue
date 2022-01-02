8f83b4f8680342d8e850a70a082332eb8ab01c4e
milkCXB <1436296421@qq.com>
2022-01-02 11:49:44 +0800

    添加

---
+++ src/pages/start.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style lang="less" scoped>
+
+</style>
