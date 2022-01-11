0ff91c04206744c3bcc314661d4468cda0c9ce32
milkCXB <1436296421@qq.com>
2022-01-11 23:55:57 +0800

    首页开发

--- src/components/topNavBar/index.vue
+++ src/components/topNavBar/index.vue
@@ -1,46 +1,54 @@
 <template>
-    <div class="bar">
-        我的博客demo
-    </div>
-    <div v-if="isBlock" class="block"></div>
+
+  <div class="bar">
+      <img class="icon" src="/img/milk-icon.png" alt="">
+      我的博客demo
+      <img class="icon" src="/img/open-icon.png" alt="">
+      </div>
+  <div v-if="isBlock" class="block"></div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent } from "vue";
 
 export default defineComponent({
-    props:{
-      isBlock:{
-          type:Boolean,
-          default:true
-      }
+  props: {
+    isBlock: {
+      type: Boolean,
+      default: true,
     },
-    setup(props:any) {
-        const isBlock = computed(() => props.isBlock)
-
-        return {
-            isBlock
-        }
-    },
-})
+  },
+  setup(props: any) {
+    const isBlock = computed(() => props.isBlock);
+    return {
+      isBlock,
+    };
+  },
+});
 </script>
 
 
 <style lang="less" scoped>
-.bar{
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    background: rgb(231, 231, 231);
-    width: 100%;
-    height:100px;
-    // box-shadow: -5px -10px gray;
+.bar {
+//   padding:0 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(233, 236, 241, 0.8);
+  width: 100%;
+  height: 120px;
+
+  .icon{
+      margin:0 40px;
+      width:80px;
+      height: 80px;
+  }
 }
 
-.block{
-    height:100px;
+.block {
+  height: 120px;
 }
 </style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,37 +1,87 @@
 <template>
+  <div class="container">
     <topNavBar></topNavBar>
-    <div class="container">
-        <div class="content" v-for="item in 12">
-            {{item}} 芜湖
+    <div class="content">
+      <div class="cover"></div>
+      <div class="card">
+        <div class="title">我是个标题</div>
+        <div class="item-box">
+          <div class="item" v-for="(item, index) in 10"></div>
         </div>
-    
+      </div>
     </div>
-    <bottomNavBar></bottomNavBar>
+    <!-- <div class="content">
+            <div class="card" >
+                <div class="card-title">{{index}}</div>
+                <div class="card-item"></div>
+            </div>
+        
+        
+        </div> -->
+  </div>
+  <!-- <bottomNavBar></bottomNavBar> -->
 </template>
 
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import topNavBar from '../components/topNavBar/index.vue'
-import bottomNavBar from '../components/bottomNavBar/index.vue'
+import { defineComponent } from "vue";
+import topNavBar from "../components/topNavBar/index.vue";
+import bottomNavBar from "../components/bottomNavBar/index.vue";
 
 export default defineComponent({
-    components:{
-        topNavBar,
-        bottomNavBar
-    },
-    setup() {
-        console.log('HOME');
-    },
-})
+  components: {
+    topNavBar,
+    bottomNavBar,
+  },
+  setup() {
+    console.log("HOME");
+  },
+});
 </script>
 
 <style lang="less" scoped>
-.content{
-    padding:50px;
-    border-radius: 20px;
-    border: 1px solid gold;
-
+.container {
+  background: url("./img/main-bc.png") no-repeat;
+  //   padding: 40px;
+  // background: #f0f0f4;
 }
 
+.content {
+  padding: 40px;
+}
+
+.cover {
+  height: 442px;
+  background: #ffffff;
+  box-shadow: -1px 10px 61px 12px #dbdbdb;
+  border-radius: 10px 10px 10px 10px;
+  opacity: 1;
+  margin-bottom: 64px;
+}
+
+.card {
+  .title {
+    font-size: 38px;
+    font-weight: bold;
+    margin-bottom: 30px;
+  }
+
+  .item-box {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    .item {
+      //   width: 314px;
+      width: 48%;
+      height: 300px;
+      margin-bottom: 30px;
+      background: #fffaf9;
+      box-shadow: 0px 3px 24px 3px #cacaca;
+      border-radius: 10px 10px 10px 10px;
+      opacity: 1;
+      // width: 100px;
+    }
+  }
+}
 </style>
